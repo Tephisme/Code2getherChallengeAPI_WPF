ada478acd72c334ab635dcd703f909eb0519331a
Tephisme <48565929+Tephisme@users.noreply.github.com>
2021-02-11 14:51:35 +0100

    Added App.cs

---
+++ Code2getherChallengeAPI.Core/Code2getherChallengeAPI.Core/App.cs
@@ -0,0 +1,13 @@
+﻿using Code2getherChallengeAPI.Core.ViewModels;
+using MvvmCross.ViewModels;
+
+namespace Code2getherChallengeAPI.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            RegisterAppStart<AppViewModel>();
+        }
+    }
+}

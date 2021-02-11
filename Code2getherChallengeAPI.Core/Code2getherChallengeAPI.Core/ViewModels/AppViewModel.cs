fdd980da9d49beb5d27cecec6fc51a75fa4bc0f4
Tephisme <48565929+Tephisme@users.noreply.github.com>
2021-02-11 14:45:48 +0100

    Added AppViewModel Logic

--- Code2getherChallengeAPI.Core/Code2getherChallengeAPI.Core/ViewModels/AppViewModel.cs
+++ Code2getherChallengeAPI.Core/Code2getherChallengeAPI.Core/ViewModels/AppViewModel.cs
@@ -1,7 +1,56 @@
-﻿namespace Code2getherChallengeAPI.Core.ViewModels
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MvvmCross.ViewModels;
+
+namespace Code2getherChallengeAPI.Core.ViewModels
 {
-    public class AppViewModel
+    public class AppViewModel :MvxViewModel
     {
-        
+        private string _imageUrl = "";
+        private readonly HttpClient _client = new HttpClient();
+
+        public string ImageUrl
+        {
+            get => _imageUrl;
+            set => SetProperty(ref _imageUrl, GetNewImageUrl());
+        }
+
+        private string GetNewImageUrl()
+        {
+            string url = "";
+
+            string responseBody = ContactAPI().ToString();
+
+            string[] responseBodies = responseBody?.Split('\"');
+
+            foreach (string response in responseBodies)
+            {
+                if (response.StartsWith("http"))
+                    url = response;
+            }
+
+            return url;
+        }
+
+        private async Task<string> ContactAPI()
+        {
+            try
+            {
+                HttpResponseMessage response = await _client
+                    .GetAsync("https://api.thecatapi.com/v1/images/search");
+
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                
+                return responseBody;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
     }
 }

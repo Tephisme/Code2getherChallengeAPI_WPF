dcaf1bbc1735b3f4e30331ca9abc253db2026b18
Tephisme <48565929+Tephisme@users.noreply.github.com>
2021-02-11 14:25:15 +0100

    Modify Views directory and file name
    -Modify Views directory and file name to Models and AppModel.cs
    -Added property ImageUrl in AppModel.cs

---
+++ Code2getherChallengeAPI.Core/Code2getherChallengeAPI.Core/Models/AppModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Code2getherChallengeAPI.Core.Views
+{
+    public class AppModel
+    {
+        public string ImageUrl { get; set; }
+    }
+}
--- Code2getherChallengeAPI.Core/Code2getherChallengeAPI.Core/Views/AppView.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Code2getherChallengeAPI.Core.Views
-{
-    public class AppView
-    {
-        
-    }
-}

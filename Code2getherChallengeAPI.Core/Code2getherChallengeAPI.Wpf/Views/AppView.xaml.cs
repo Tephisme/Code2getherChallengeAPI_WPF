e79d5136867a0421ec6959119660019914f5e305
Tephisme <48565929+Tephisme@users.noreply.github.com>
2021-02-12 07:50:19 +0100

    Created AppView.cs

---
+++ Code2getherChallengeAPI.Core/Code2getherChallengeAPI.Wpf/Views/AppView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Code2getherChallengeAPI.Wpf.Views
+{
+    public partial class AppView : UserControl
+    {
+        public AppView()
+        {
+            InitializeComponent();
+        }
+    }
+}
